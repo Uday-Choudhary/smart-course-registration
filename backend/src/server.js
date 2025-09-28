dd37714ce3d137ccb7ebe44a61a668b0611077c5
prempyla <pylaprem@gmail.com>
2025-09-29 01:15:47 +0530

    project setup

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
+
+const app=express()
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.status.apply(200).json({mes:"api is running"})
+})
+
+
+const PORT=process.env.PORT || 4000;
+app.listen(PORT,()=>console.log(`backend running on port ${PORT}`))
