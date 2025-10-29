ab1178a876bf5ef11814ca8a9e96cb35a577d5a1
Uday-Choudhary <choudharyuday85@gmail.com>
2025-10-29 22:59:44 +0530

    feat: add initial routing setup with login, register, and dashboard placeholders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import LoginPage from "./pages/Auth/LoginPage";
+import RegisterPage from "./pages/Auth/RegisterPage";
+import StudentDashboard from "./components/dashboard/StudentDashboard";
+import FacultyDashboard from "./components/dashboard/FacultyDashboard";
+import AdminDashboard from "./components/dashboard/AdminDashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        
+        {/* Auth Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        {/* Dashboard Routes */}
+        <Route path="/student" element={<StudentDashboard />} />
+        <Route path="/faculty" element={<FacultyDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        
       </Routes>
     </BrowserRouter>
   );
